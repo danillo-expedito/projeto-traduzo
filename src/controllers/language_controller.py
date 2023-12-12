b97c7e5b593a9fcad5e7b09c65df2718b906915e
Danillo Santos <dnashy@hotmail.com>
2023-12-12 12:31:51 -0300

    fix

--- src/controllers/language_controller.py
+++ src/controllers/language_controller.py
@@ -35,3 +35,26 @@ def language_list():
         translate_to=translate_to,
         translated=translated,
     )
+
+
+@language_controller.route("/reverse", methods=["POST"])
+def reverse_translate():
+    languages = LanguageModel.list_dicts()
+
+    text_to_translate = request.form.get("text-to-translate")
+    translate_from = request.form.get("translate-from")
+    translate_to = request.form.get("translate-to")
+    translated = GoogleTranslator(
+        source=str(translate_from),
+        target=str(translate_to)).translate(
+        str(text_to_translate)
+    )
+
+    return render_template(
+        "index.html",
+        languages=languages,
+        text_to_translate=translated,
+        translate_from=translate_to,
+        translate_to=translate_from,
+        translated=text_to_translate,
+    )
