5b0bdcabf780e2c345bc59ea04ef8d2a541989aa
Danillo Santos <dnashy@hotmail.com>
2023-12-12 12:01:22 -0300

    feat: add the GoogleTranslator API to the language_controller

--- src/controllers/language_controller.py
+++ src/controllers/language_controller.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, render_template
 from models.language_model import LanguageModel
+from deep_translator import GoogleTranslator
 
 language_controller = Blueprint("language_controller", __name__)
 
@@ -7,11 +8,24 @@ language_controller = Blueprint("language_controller", __name__)
 @language_controller.route("/", methods=["GET", "POST"])
 def language_list():
     languages = LanguageModel.list_dicts()
-    text_to_translate = (
-        request.form.get("text-to-translate") or "O que deseja traduzir?")
-    translate_from = request.form.get("translate-from") or "pt"
-    translate_to = request.form.get("translate-to") or "en"
-    translated = "What do you want to translate?"
+
+    if request.method == "POST":
+        text_to_translate = request.form.get("text-to-translate")
+        translate_from = request.form.get("translate-from")
+        translate_to = request.form.get("translate-to")
+
+        translated = GoogleTranslator(
+            source=str(translate_from),
+            target=str(translate_to)).translate(
+            str(text_to_translate)
+        )
+    else:
+        text_to_translate = (
+            request.form.get("text-to-translate") or "O que deseja traduzir?")
+
+        translate_from = request.form.get("translate-from") or "pt"
+        translate_to = request.form.get("translate-to") or "en"
+        translated = "What do you want to translate?"
 
     return render_template(
         "index.html",
